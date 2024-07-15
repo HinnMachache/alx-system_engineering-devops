50137d43637a14c86a6ba21714e29055cb93f58e
HinnMachache <bennyhinn253.bh@gmail.com>
2024-07-15 15:06:40 -0700

    Corrections on API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+
 """
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0.
+
+Hint: No authentication is necessary for most features of the Reddit API.
+If you’re getting errors related to Too Many Requests, ensure you’re
+setting a custom User-Agent.
 """
+
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-
-    headers = {
-        'User-Agent': u_agent
-    }
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+    """Return the number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return (response.json().get('data').get('subscribers'))
+    return (0)
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
+
 """
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
+function that queries the Reddit API and prints the titles of the first 10 hot
+posts listed for a given subreddit.
 """
+
+
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-
-    headers = {
-        'User-Agent': u_agent
-    }
-
-    params = {
-        'limit': 10
-    }
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
-        return
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
+    """Return the first 10 hot posts listed for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        for post in response.json().get('data').get('children'):
+            print(post.get('data').get('title'))
     else:
-        for post in hot_posts:
-            print(post['data']['title'])
+        print(None)
