fadad49b11dccf88b3edbbe1cff60dae2c464f32
HinnMachache <bennyhinn253.bh@gmail.com>
2024-07-15 20:45:43 -0700

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-
 """
-function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit. If an invalid
-subreddit is given, the function should return 0.
-
-Hint: No authentication is necessary for most features of the Reddit API.
-If you’re getting errors related to Too Many Requests, ensure you’re
-setting a custom User-Agent.
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return (response.json().get('data').get('subscribers'))
-    return (0)
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-
-"""
-function that queries the Reddit API and prints the titles of the first 10 hot
-posts listed for a given subreddit.
-"""
-
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Return the first 10 hot posts listed for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        for post in response.json().get('data').get('children'):
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
             print(post.get('data').get('title'))
-    else:
+    except Exception:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
